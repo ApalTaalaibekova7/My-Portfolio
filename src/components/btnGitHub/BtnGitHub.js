e2dda445783aee3d47a020df391c6f037bb1769a
ApalTaalaibekova7 <apaltaalaibekova7@gmail.com>
2025-08-17 21:03:27 +0300

    Обновила проект

---
+++ src/components/btnGitHub/BtnGitHub.js
@@ -0,0 +1,14 @@
+import './style.css';
+// import img from "../../img/projects/Nike.jpg";
+import gitHubIcon from "../../img/icons/fıgma.png";
+
+function BtnGitHub({link}) {
+    return (
+        <a href={link} target="_blank" rel="noreferrer" className="btn-outline">
+            <img className='figma-icon' src={gitHubIcon} alt="Figma"/>
+            Open 
+        </a>
+    )
+}
+
+export default BtnGitHub;
---
+++ src/components/header/Header.js
@@ -0,0 +1,21 @@
+import './style.css'
+
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header__wrapper">
+                <h1 className="header__title">
+                    <strong>Hi, my name is <em>Apal</em></strong><br/>
+                    I am a UX/UI Designer
+                </h1>
+                <div className="header__text">
+                    <p>With passion for learning and creating.</p>
+                </div>
+                <a href="https://drive.google.com/file/d/1sCmduq1tKgGgPUjN_az2ubGLfaRhwG-Q/view" className="btn">Open CV</a>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
